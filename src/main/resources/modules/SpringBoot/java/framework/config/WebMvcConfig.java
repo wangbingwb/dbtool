32c0b6acfe77f6b76f75231cabd243658e493799
wangbing <wangbingit@outlook.com>
2018-08-20 22:33:13 +0800

    1、配置文件 2、banner

--- src/main/resources/modules/SpringBoot/java/framework/config/WebMvcConfig.java
+++ src/main/resources/modules/SpringBoot/java/framework/config/WebMvcConfig.java
@@ -2,10 +2,8 @@ package ${basePackage}.framework.config;
 
 import ${basePackage}.framework.springmvc.GlobalHandlerInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -20,9 +18,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     }
 
     @Autowired
-    private CharacterEncodingFilter characterEncodingFilter;
-
-    @Autowired
     private GlobalHandlerInterceptor globalHandlerInterceptor;
 
     @Override
@@ -31,27 +26,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(globalHandlerInterceptor).addPathPatterns("**");
     }
 
-    @Bean
-    public FilterRegistrationBean characterEncodingFilterRegistration() {
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        //注入过滤器
-        registration.setFilter(characterEncodingFilter);
-        //拦截规则
-        registration.addUrlPatterns("/*");
-        //过滤器名称
-        registration.setName("CharacterEncodingFilter");
-        //过滤器顺序
-        registration.setOrder(1);
-        return registration;
-    }
-
-    @Bean
-    public CharacterEncodingFilter characterEncodingFilter() {
-        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-        characterEncodingFilter.setEncoding("UTF-8");
-        characterEncodingFilter.setForceEncoding(true);
-        return characterEncodingFilter;
-    }
     @Bean
     public GlobalHandlerInterceptor globalHandlerInterceptor() {
         GlobalHandlerInterceptor globalHandlerInterceptor = new GlobalHandlerInterceptor();
