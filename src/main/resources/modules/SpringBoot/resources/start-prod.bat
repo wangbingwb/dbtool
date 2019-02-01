8774f84fb97ff3e002f698012615db6d7e94b4e0
wangbing <wangbingit@outlook.com>
2019-02-02 02:09:52 +0800

    1、支持https

--- src/main/resources/modules/SpringBoot/resources/start-prod.bat
+++ src/main/resources/modules/SpringBoot/resources/start-prod.bat
@@ -1,3 +1,3 @@
 @echo off
 cd %cd%
-start java -Dfile.encoding=UTF-8 -jar ${projectName}-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev
+start java -Dfile.encoding=UTF-8 -jar ${projectName}-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod
